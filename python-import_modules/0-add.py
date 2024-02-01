8bad19c40314a25cf2a21307ba730d041ac6e2a6
Caspyk <150113179+Caspyk@users.noreply.github.com>
2024-02-01 14:23:43 +0200

    Create python-package-conda.yml

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+    a = 1
+    b = 2
+def add(a, b):
+    result = 1 + 2
+    print(f"The sum of {a} and {b} is: {result}") 
+    
---
+++ python-import_modules/README.md
@@ -0,0 +1,1 @@
+Create python-package-conda.yml
