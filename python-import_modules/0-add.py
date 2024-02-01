5af76df0191ab68591630c42db1a46216ccd1087
Caspyk <150113179+Caspyk@users.noreply.github.com>
2024-02-01 14:39:28 +0200

    Create python-package-conda.yml

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -5,5 +5,10 @@ def add(a, b):
     result = 1 + 2
     print(f"The sum of {a} and {b} is: {result}")  
 if __name__ == "__main__":
-    add(1, 2)
+    add(1, 2) 
+    a = 1
+    b = 2
+    result = 3
+    print("{} + {} = {}".format(a, b, c))
+
     
